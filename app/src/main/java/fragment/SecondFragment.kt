862a38f12b5c96ab34e6a915ef00c87283f6e4d9
kimhaun <wha02068@gmail.com>
2020-11-14 14:41:39 +0900

    두번째 프래그먼트 생성 (동작코드는 없음)

---
+++ app/src/main/java/fragment/SecondFragment.kt
@@ -0,0 +1,30 @@
+package fragment
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.appisna.blogspot.fragment_20201114.R
+import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.android.synthetic.main.fragment_second.*
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        alertBtn.setOnClickListener {
+
+        }
+    }
+}
