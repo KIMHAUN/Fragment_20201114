d00a82ecf40c6b8b13280b8672e905ba788e7269
kimhaun <wha02068@gmail.com>
2020-11-14 13:57:11 +0900

    첫번째 프래그먼트 코틀린파일 추가. + xml과 연결

---
+++ app/src/main/java/fragment/FistFragment.kt
@@ -0,0 +1,22 @@
+package fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import com.appisna.blogspot.fragment_20201114.R
+
+//액티비티에 올라갈 조각
+class FistFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+}
